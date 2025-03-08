df968a83e0dda8624923ed8c67e282282f5a88c7
Steven-Ou <osteve425@gmail.com>
2025-03-08 10:20:06 -0500

    changing it to paragarph

--- ever/app/page.js
+++ ever/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
       <br/>
       <Box>
        <nav className="jian">
-        <a href="app/page.js">Welcome(home)</a>
+        <a href="/page.js">Welcome</a>
         <a href="info/infoPage.js">Information(home)</a>
         <a href="faq/faqPage.js">FAQ(home)</a>
        </nav>
@@ -21,14 +21,14 @@ export default function Home() {
       <Box sx={{textAlign:"center",display:"flex"}}>
         <Box>
           <h1 className="xu">OUR MISSION:</h1>
-          <h2>EverGreen aims to provide users with real-time air pollution data in parks, forests, and across the United States. 
-            By highlighting these vulnerable and endangered regions, we hope to engage environmental activists as well as inform policymakers in their efforts to reduce climate change.</h2>
+          <p>EverGreen aims to provide users with real-time air pollution data in parks, forests, and across the United States. 
+            By highlighting these vulnerable and endangered regions, we hope to engage environmental activists as well as inform policymakers in their efforts to reduce climate change.</p>
         </Box>
         <Box>
           <h1 className="xu">OUR VISION:</h1>
-          <h2>Our ultimate goal is twofold: On one hand, we wish to provide policymakers with the necessary resources to make informed decisions for the betterment of their districts. 
+          <p>Our ultimate goal is twofold: On one hand, we wish to provide policymakers with the necessary resources to make informed decisions for the betterment of their districts. 
             On the other hand, we would like to mobilize our users by having them be more conscious of environmental conditions in their communities. 
-            All together, we hope that our efforts will contribute to the reduction of air pollution across the nation.</h2>
+            All together, we hope that our efforts will contribute to the reduction of air pollution across the nation.</p>
         </Box>
       </Box>
     </Box>
