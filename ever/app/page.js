59fdbb28312a042c30e2d044c6cc2f60b8651d62
Steven-Ou <osteve425@gmail.com>
2025-03-08 12:07:39 -0500

    push

--- ever/app/page.js
+++ ever/app/page.js
@@ -46,7 +46,7 @@ export default function Home() {
         </Box>
         <hr/>
         <footer>
-        <p>Website Design by: | Harrison Min, Steven Ou, Allen Singleton| Queens College | Copyright © 2022 </p>
+        <p>Website Design by: | Harrison Min, Steven Ou, Allen Singleton| Queens College | Copyright © 2025 </p>
         </footer>
 
     </Box>
