0432bcab63741488c705f4e3604b9ff9c3e0afb8
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-03-08 08:13:05 -0500

    Box

--- ever/app/page.js
+++ ever/app/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import * as React from 'react';
+import { Box } from "@mui/material";
+
 export default function Home() {
-  return (
-    <Box>
-
-    </Box>
-
+  return (  
+    <Box></Box>
   );
 }
