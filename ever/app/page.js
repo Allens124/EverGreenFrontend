3b11023451c8dc782cba774c9bda97026b8d40a7
Steven <osteve425@gmail.com>
2025-03-08 08:18:01 -0500

    updating with link

--- ever/app/page.js
+++ ever/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import * as React from 'react';
-import { Box } from "@mui/material";
+import { Box, Link } from "@mui/material";
 
 export default function Home() {
   return (  
-    <Box display ={"flex"} font={"30px"}>
-      
+    <Box display={"flex"} font={"30px"}>
+      <Link href="/">Welcome!</Link>
+      <Link href="/info">Information</Link>
+      <Link href="/home">AI Assistant</Link>
     </Box>
   );
 }
