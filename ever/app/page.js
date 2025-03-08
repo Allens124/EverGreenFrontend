e192f4510c438dc3b698f7ddd37cc5525d645826
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-03-08 08:32:46 -0500

    creating sx

--- ever/app/page.js
+++ ever/app/page.js
@@ -6,7 +6,12 @@ import { Box, Link } from "@mui/material";
 
 export default function Home() {
   return (  
-    <Box className="jian">
+    <Box sx={{flex:1},
+      {text-align:center},
+      {padding:10px},  
+      {text-decoration:none},
+      {font-size:21px},
+      {color:black}}>
       <Link href="/">Welcome!</Link>
       <Link href="/info">Information</Link>
       <Link href="/home">AI Assistant</Link>
