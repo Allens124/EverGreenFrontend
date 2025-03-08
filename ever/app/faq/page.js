0d65e2d73e0400d284f9f49195b6b2f7688cd499
Steven-Ou <osteve425@gmail.com>
2025-03-08 13:43:45 -0500

    subsituting

--- ever/app/faq/page.js
+++ ever/app/faq/page.js
@@ -1,5 +1,5 @@
 'use client'
-import Image from "next/image";
+import { useState } from "react";
 import "../globals.css";
 import * as React from 'react';
 import { Box, Button, Stack, TextField, Link } from "@mui/material";
