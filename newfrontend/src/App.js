fa80cad6adaa2fe0fc1a0d9243b959dace1597f0
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-03-09 01:01:45 -0500

    syncing

--- newfrontend/src/App.js
+++ newfrontend/src/App.js
@@ -8,14 +8,14 @@ function App() {
     <Router>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/map">Map</Link>
-        <Link to="/faq">FAQ</Link>
+        <Link to="/MapPage">Map</Link>
+        <Link to="/FAQPage">FAQ</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/map" element={<MapPage />} />
-        <Route path="/faq" element={<FAQPage />} />
+        <Route path="/MapPage" element={<MapPage />} />
+        <Route path="/FAQPage" element={<FAQPage />} />
       </Routes>
     </Router>
   );
--- newfrontend/src/components/FAQPage.js
+++ newfrontend/src/components/FAQPage.js
@@ -20,30 +20,22 @@ import * as React from 'react';
 import { Box, Button } from "@mui/material";
 
 export default function Home() {
-        <Box>  
-          <Box>  
-            <Box>
-              <a href="/">HOME</a>
-              <a href="/map">EVERGREEN MAP</a>
-              <a href="/faq">FAQ</a>
-            </Box>
-        </Box>
+        <><Box>
+    <Box>
+      <Box>
+        <a href="/">HOME</a>
+        <a href="/map">EVERGREEN MAP</a>
+        <a href="/faq">FAQ</a>
       </Box>
-        <h1 style={{textAlign:"center"}}>FAQ</h1>
-        <hr/>
-          <Box className="b">
-            <h1 className="xu">Q: </h1>
-            <p>A: </p>
-          </Box>
-          <hr/>
-          <Box className="b">
-            <h1 className="xu">Q: </h1>
-            <p>A: </p>
-          </Box>
-          <hr/>
-          <Box className="b">
-            <h1 className="xu">Can I integrate my own API keys for additional environmental data?</h1>
-            <p>Currently, EverGreen supports API integrations for trees, parks, forests, and air quality. We are actively working <br/> on expanding our platform to include lakes, oceans, and wildlife data in the future. Stay tuned for updates!</p>
-          </Box>
-          </Box>
+    </Box>
+  </Box><h1 style={{ textAlign: "center" }}>FAQ</h1><hr /><Box className="b">
+      <h1 className="xu">Q: </h1>
+      <p>A: </p>
+    </Box><hr /><Box className="b">
+      <h1 className="xu">Q: </h1>
+      <p>A: </p>
+    </Box><hr /><Box className="b">
+      <h1 className="xu">Can I integrate my own API keys for additional environmental data?</h1>
+      <p>Currently, EverGreen supports API integrations for trees, parks, forests, and air quality. We are actively working <br /> on expanding our platform to include lakes, oceans, and wildlife data in the future. Stay tuned for updates!</p>
+    </Box></>
 }
