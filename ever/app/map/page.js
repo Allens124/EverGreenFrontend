2a8e7755c6e2d38d9cae52f90a30ce63a76d59fe
Steven-Ou <osteve425@gmail.com>
2025-03-09 00:33:30 -0500

    fixed

--- ever/app/map/page.js
+++ ever/app/map/page.js
@@ -34,35 +34,5 @@ export function Home() {
     </Box>
     
   );
-
-export default function Home() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  return (  
-    <Box className="luck">
-      <Box className="up">
-        <Box >
-        <h1 className="tit">EverGreen</h1>
-        </Box>
-        {/* Menu Button */}
-        <Box>
-          <Button type="button" className="toggle" color="black" onClick={() => setMenuOpen(prev => !prev)} >  
-            ☰
-          </Button>
-          {/* Dropdown Menu Box */}
-          {menuOpen && (
-            <Box className="menub">
-              <a href="/">HOME</a>
-              <a href="/info">WHO WE ARE</a>
-              <a href="/map">EVERGREEN MAP</a>
-              <a href="/faq">FAQ</a>
-            </Box>
-          )}
-        </Box>
-      </Box>
-        <h1 style={{textAlign:"center"}}>EVERGREEN MAP</h1>
-        <hr/>
-      </Box>
-  )};
-     
 };
 
