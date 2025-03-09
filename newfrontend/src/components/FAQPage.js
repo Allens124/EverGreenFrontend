fbd54000522584c984da037ef46ac6b349ec3ff2
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-03-09 00:44:31 -0500

    syncing

--- newfrontend/src/components/FAQPage.js
+++ newfrontend/src/components/FAQPage.js
@@ -20,26 +20,13 @@ import * as React from 'react';
 import { Box, Button } from "@mui/material";
 
 export default function Home() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  return (  
-    <Box className="luck">
-      <Box className="up">
-        <Box >
-        <h1 className="tit">EverGreen</h1>
-        </Box>
-        {/* Menu Button */}
-        <Box>
-          <Button type="button" className="toggle" color="black" onClick={() => setMenuOpen(prev => !prev)} >  
-            ☰
-          </Button>
-          {/* Dropdown Menu Box */}
-          {menuOpen && (
-            <Box className="menub">
+        <Box>  
+          <Box>  
+            <Box>
               <a href="/">HOME</a>
               <a href="/map">EVERGREEN MAP</a>
               <a href="/faq">FAQ</a>
             </Box>
-          )}
         </Box>
       </Box>
         <h1 style={{textAlign:"center"}}>FAQ</h1>
@@ -58,6 +45,5 @@ export default function Home() {
             <h1 className="xu">Can I integrate my own API keys for additional environmental data?</h1>
             <p>Currently, EverGreen supports API integrations for trees, parks, forests, and air quality. We are actively working <br/> on expanding our platform to include lakes, oceans, and wildlife data in the future. Stay tuned for updates!</p>
           </Box>
-    </Box>
-  );
+          </Box>
 }
